1b9cf0b148fb1e341d43b20863f1ba45f59505b0
Kyle Español <kylekce@gmail.com>
2024-04-09 06:48:54 +0000

    Apr 9, 2024, 2:48 PM

--- Week 0/cyberchase/8.sql
+++ Week 0/cyberchase/8.sql
@@ -1,1 +1,1 @@
-SELECT count("id") FROM "episodes" WHERE "year" > '2018-01-01' AND "year" < '2023-12-31';
+SELECT count("id") FROM "episodes" WHERE "air_date" > '2018-01-01' AND "air_date" < '2023-12-31';
