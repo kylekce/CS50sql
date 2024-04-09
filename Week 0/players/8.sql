dfceb4722496acb5a6f4d85aaa3a8606de386a0f
Kyle Español <kylekce@gmail.com>
2024-04-09 16:44:42 +0000

    Apr 10, 2024, 12:44 AM

--- Week 0/players/8.sql
+++ Week 0/players/8.sql
@@ -1,3 +1,3 @@
-SELECT ROUND(AVG("height"), 2), ROUND(AVG("weight"), 2)
+SELECT ROUND(AVG("height"), 2) as, ROUND(AVG("weight"), 2)
 FROM "players"
 WHERE "debut" >= '2000-01-01' AND "throws" = 'R';
