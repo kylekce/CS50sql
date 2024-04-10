31f7db7b4b5064fcc1782066e77727b65200baa5
Kyle Español <kylekce@gmail.com>
2024-04-10 15:28:26 +0000

    Apr 10, 2024, 11:28 PM

--- Week 1/dese/11.sql
+++ Week 1/dese/11.sql
@@ -1,3 +1,5 @@
-SELECT "name", "expenditures"."per_pupil_expenditure"
+SELECT "name", "expenditures"."per_pupil_expenditure", "graduation_rates"."graduated"
 FROM "schools"
-JOIN "expenditures" ON "schools"."district_id" = "expenditures"."district_id";
+JOIN "graduation_rates" ON "schools"."id" = "graduation_rates"."school_id"
+JOIN "expenditures" ON "schools"."district_id" = "expenditures"."district_id"
+ORDER BY "expenditures"."per_pupil_expenditure" DESC, "name" ASC;
