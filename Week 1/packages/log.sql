8eeb0319adb7f8a17ca32b8600d23a7518975f46
Kyle Español <kylekce@gmail.com>
2024-04-10 12:43:47 +0000

    Apr 10, 2024, 8:43 PM

--- Week 1/packages/log.sql
+++ Week 1/packages/log.sql
@@ -28,14 +28,18 @@ WHERE "id" = (
 -- Address: "address" = "2 Finnigan Street"
 
 -- *** The Devious Delivery ***
-SELECT "id", "contents", "to_address_id"
+SELECT "id", "contents"
 FROM "packages"
 WHERE "from_address_id" IS NULL;
--- Package: "id" = 5098, "contents" = "Duck debugger", "to_address_id" = 50
+-- Package: "id" = 5098, "contents" = "Duck debugger"
 
 SELECT "address", "type"
 FROM "addresses"
-WHERE "id" = 50;
+WHERE "id" = (
+    SELECT "address_id"
+    FROM "scans"
+    WHERE "id" = 5098 AND "action" = 'Drop'
+);
 -- Address: "address" = "123 Sesame Street", "type" = "Residential"
 
 -- *** The Forgotten Gift ***
