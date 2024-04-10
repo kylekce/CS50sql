ffca1be4361e09063908e21c584c62817d1ab140
Kyle Español <kylekce@gmail.com>
2024-04-10 14:57:48 +0000

    Apr 10, 2024, 10:57 PM

--- Week 1/dese/7.sql
+++ Week 1/dese/7.sql
@@ -1,5 +1,8 @@
 SELECT "name"
 FROM "schools"
-JOIN "graduation_rates" ON "schools"."id" = "graduation_rates"."school_id"
-WHERE ("type" = 'Public School' OR "type" = 'Charter School') AND 
-AND "graduation_rates"."graduated" = 100;
+WHERE ("type" = 'Public School' OR "type" = 'Charter School') 
+AND "district_id" = (
+    SELECT "id"
+    FROM "districts"
+    WHERE "name" = 'Cambridge'
+);
