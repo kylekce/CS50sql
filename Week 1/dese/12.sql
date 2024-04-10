796a2fbc7ccd6a564bd709fb3d81a6993e633123
Kyle Español <kylekce@gmail.com>
2024-04-10 15:35:16 +0000

    Apr 10, 2024, 11:35 PM

--- Week 1/dese/12.sql
+++ Week 1/dese/12.sql
@@ -3,6 +3,12 @@ FROM "districts"
 JOIN "expenditures" ON "districts"."id" = "expenditures"."district_id"
 JOIN "staff_evaluations" ON "districts"."id" = "staff_evaluations"."district_id"
 WHERE "type" = 'Public School District' 
-AND "expenditures"."per_pupil_expenditure" > AVG("expenditures"."per_pupil_expenditure")
-AND "staff_evaluations"."exemplary" > AVG("staff_evaluations"."exemplary")
+AND "expenditures"."per_pupil_expenditure" > (
+    SELECT AVG("per_pupil_expenditure")
+    FROM "expenditures"
+    )
+AND "staff_evaluations"."exemplary" > (
+    SELECT AVG("staff_evaluations"."exemplary")
+    FROM "staff_evaluations"
+    )
 ORDER BY "staff_evaluations"."exemplary" DESC, "expenditures"."per_pupil_expenditure" DESC;
