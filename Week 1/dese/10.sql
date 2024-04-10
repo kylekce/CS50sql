59a8a088d2f9c4c73b30755aff061315ea214f4a
Kyle Español <kylekce@gmail.com>
2024-04-10 15:21:24 +0000

    Apr 10, 2024, 11:21 PM

--- Week 1/dese/10.sql
+++ Week 1/dese/10.sql
@@ -1,5 +1,6 @@
 SELECT "districts"."name", "per_pupil_expenditure"
 FROM "expenditures"
 JOIN "districts" ON "expenditures"."district_id" = "districts"."id"
+WHERE "districts"."type" = 'Public School District'
 ORDER BY "per_pupil_expenditure" DESC
 LIMIT 10;
