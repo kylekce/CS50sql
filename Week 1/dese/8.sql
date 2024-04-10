b4c51ef67fde8ff3fec4e85dafff57456d91e454
Kyle Español <kylekce@gmail.com>
2024-04-10 15:04:39 +0000

    Apr 10, 2024, 11:04 PM

--- Week 1/dese/8.sql
+++ Week 1/dese/8.sql
@@ -1,4 +1,4 @@
-SELECT "name", COUNT("expenditures"."pupils") AS "Number of Pupils"
+SELECT *
 FROM "districts"
 JOIN "expenditures" ON "districts"."id" = "expenditures"."district_id"
 GROUP BY "districts"."name";
