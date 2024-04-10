9679825ebe9c04fe10d3def6d7fef08b30684833
Kyle Español <kylekce@gmail.com>
2024-04-10 13:53:57 +0000

    Apr 10, 2024, 9:53 PM

--- Week 1/dese/1.sql
+++ Week 1/dese/1.sql
@@ -1,3 +1,4 @@
 SELECT DISTINCT "name", "city"
 FROM "schools"
-WHERE "type" = 'Public School' OR "type" = 'Charter School';
+WHERE "type" = 'Public School' OR "type" = 'Charter School'
+ORDER BY "name" ASC;
