4f1196720a92c688a1fcb61ff1abe758aa01709d
Kyle Español <kylekce@gmail.com>
2024-04-10 12:16:52 +0000

    Apr 10, 2024, 8:16 PM

--- Week 1/packages/log.sql
+++ Week 1/packages/log.sql
@@ -1,6 +1,12 @@
 
 -- *** The Lost Letter ***
-SELECT * FROM "packages" JOIN "addresses" ON "packages"."from_address_id" = "addresses"."id";
+SELECT "id", "contents" 
+FROM "packages" 
+WHERE "from_address_id" = (
+    SELECT "id" 
+    FROM "addresses" 
+    WHERE "address" = '900 Somerville Avenue'
+);
 
 -- *** The Devious Delivery ***
 
