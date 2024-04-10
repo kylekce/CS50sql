c6b6b2aa64f90d248363924dd71890525d55496d
Kyle Español <kylekce@gmail.com>
2024-04-10 12:22:31 +0000

    Apr 10, 2024, 8:22 PM

--- Week 1/packages/log.sql
+++ Week 1/packages/log.sql
@@ -1,14 +1,35 @@
 
 -- *** The Lost Letter ***
-SELECT "id", "contents" 
+SELECT "id", "contents", "from_address_id" 
 FROM "packages" 
 WHERE "from_address_id" = (
     SELECT "id" 
     FROM "addresses" 
     WHERE "address" = '900 Somerville Avenue'
 );
+-- Package: "id" = 384, "contents" = "Congratulatory letter"
+
+SELECT "type"
+FROM "addresses"
+WHERE "id" = (
+    SELECT "from_address_id"
+    FROM "packages"
+    WHERE "id" = 384
+);
+-- Type: "type" = "residential"
+
+SELECT "address"
+FROM "addresses"
+WHERE "id" = (
+    SELECT "to_address_id"
+    FROM "packages"
+    WHERE "id" = 384
+);
+-- Address: "address" = "2 Finnigan Street"
 
 -- *** The Devious Delivery ***
+
+
 
 -- *** The Forgotten Gift ***
 
