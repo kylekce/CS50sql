56e78028376466eeb6be692d9714321fe9a8dd34
Kyle Español <kylekce@gmail.com>
2024-04-10 14:50:15 +0000

    Apr 10, 2024, 10:50 PM

--- Week 1/dese/4.sql
+++ Week 1/dese/4.sql
@@ -2,5 +2,5 @@ SELECT "city", COUNT("type") as "Number of Public Schools"
 FROM "schools"
 WHERE "type" = 'Public School'
 GROUP BY "city"
-ORDER BY "Number of Public Schools" DESC
+ORDER BY "Number of Public Schools" DESC, "city" ASC
 LIMIT 10;
