f8139b596a49742267d95da590f4acf4c1bc90dc
Kyle Español <kylekce@gmail.com>
2024-04-11 11:45:40 +0000

    Apr 11, 2024, 7:45 PM

--- Week 1/moneyball/12.sql
+++ Week 1/moneyball/12.sql
@@ -2,7 +2,7 @@ SELECT "first_name", "last_name"
 FROM (
     SELECT "first_name", "last_name", "id"
     FROM (
-        SELECT "first_name", "last_name", "players"."id" as "id"
+        SELECT "first_name", "last_name", "players"."id"
         FROM "performances"
         JOIN "players" ON "performances"."player_id" = "players"."id"
         JOIN "salaries" ON "players"."id" = "salaries"."player_id"
@@ -11,20 +11,20 @@ FROM (
         ORDER BY "salary" / "H"
         LIMIT 10
     )
-)
 
-INTERSECT
+    INTERSECT
 
-SELECT "first_name", "last_name", "id"
-FROM (
-    SELECT "first_name", "last_name", "players"."id" as "id"
-    FROM "performances"
-    JOIN "players" ON "performances"."player_id" = "players"."id"
-    JOIN "salaries" ON "players"."id" = "salaries"."player_id"
-    AND "performances"."year" = "salaries"."year"
-    WHERE "performances"."year" = 2001 AND "RBI" > 0  
-    ORDER BY "salary" / "RBI"
-    LIMIT 10
+    SELECT "first_name", "last_name", "id"
+    FROM (
+        SELECT "first_name", "last_name", "players"."id"
+        FROM "performances"
+        JOIN "players" ON "performances"."player_id" = "players"."id"
+        JOIN "salaries" ON "players"."id" = "salaries"."player_id"
+        AND "performances"."year" = "salaries"."year"
+        WHERE "performances"."year" = 2001 AND "RBI" > 0  
+        ORDER BY "salary" / "RBI"
+        LIMIT 10
+    )
 )
 
 ORDER BY "id";
