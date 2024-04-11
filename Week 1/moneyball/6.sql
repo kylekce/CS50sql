b46cd04b83e5a343b1c168ba416273f8e6ada506
Kyle Español <kylekce@gmail.com>
2024-04-11 05:45:12 +0000

    Apr 11, 2024, 1:45 PM

--- Week 1/moneyball/6.sql
+++ Week 1/moneyball/6.sql
@@ -5,11 +5,3 @@ WHERE "performances"."year" = 2001
 GROUP BY "name"
 ORDER BY "total hits" DESC
 LIMIT 5;
-
--- SELECT SUM("H") AS "total hits"
--- FROM "performances"
--- WHERE "year" = 2001 AND "team_id" = (
---     SELECT "id" 
---     FROM "teams" 
---     WHERE "name" = 'Minnesota Twins'
--- );
