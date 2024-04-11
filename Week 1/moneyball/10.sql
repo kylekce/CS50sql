c8eaaf1d722aea3210ef806af8592cdac20d9151
Kyle Español <kylekce@gmail.com>
2024-04-11 07:37:11 +0000

    Apr 11, 2024, 3:37 PM

--- Week 1/moneyball/10.sql
+++ Week 1/moneyball/10.sql
@@ -1,7 +1,7 @@
--- SELECT "first_name", "last_name", "salaries"."salary", "salaries"."year", "performances"."HR"
-SELECT COUNT()
+SELECT "first_name", "last_name", "salaries"."salary", "salaries"."year", "performances"."HR"
 FROM "players"
 JOIN "salaries" ON "players"."id" = "salaries"."player_id"
 JOIN "performances" ON "salaries"."year" = "performances"."year"
 GROUP BY "players"."id", "salaries"."year", "performances"."HR"
-ORDER BY "players"."id" ASC, "salaries"."year" DESC, "performances"."HR" DESC, "salaries"."salary" DESC;
+ORDER BY "players"."id" ASC, "salaries"."year" DESC, "performances"."HR" DESC, "salaries"."salary" DESC
+LIMIT 20;
