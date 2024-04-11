228dce5d9e73d30c397b8b03771b044cc1403423
Kyle Español <kylekce@gmail.com>
2024-04-11 10:19:41 +0000

    Apr 11, 2024, 6:19 PM

--- Week 1/moneyball/10.sql
+++ Week 1/moneyball/10.sql
@@ -1,8 +1,7 @@
-SELECT DISTINCT "players"."id", "first_name", "last_name", "salaries"."salary", "salaries"."year", "performances"."HR"
+SELECT "players"."id", "first_name", "last_name", "salaries"."salary", "salaries"."year", "performances"."HR"
 FROM "players"
 JOIN "salaries" ON "players"."id" = "salaries"."player_id"
 JOIN "performances" ON "salaries"."year" = "performances"."year"
 WHERE "salaries"."year" = "performances"."year"
-GROUP BY "salaries"."year"
 ORDER BY "players"."id" ASC, "salaries"."year" DESC, "performances"."HR" DESC, "salaries"."salary" DESC
 LIMIT 10;
