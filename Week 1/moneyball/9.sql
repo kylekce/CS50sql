95f3fff9dc2463ec025b6ed17f9dd933990009bf
Kyle Español <kylekce@gmail.com>
2024-04-11 06:11:42 +0000

    Apr 11, 2024, 2:11 PM

--- Week 1/moneyball/9.sql
+++ Week 1/moneyball/9.sql
@@ -1,1 +1,7 @@
-SELECT "name"
+SELECT "name", ROUND(AVG("salary"), 2) AS "average salary"
+FROM "teams"
+JOIN "salaries" ON "teams"."id" = "salaries"."team_id"
+WHERE "salaries"."year" = 2001
+GROUP BY "name"
+ORDER BY "average salary" ASC
+LIMIT 5;
