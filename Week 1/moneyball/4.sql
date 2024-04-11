21be68ce35cbf86f72636d2a8478f033766ae374
Kyle Español <kylekce@gmail.com>
2024-04-11 04:40:37 +0000

    Apr 11, 2024, 12:40 PM

--- Week 1/moneyball/4.sql
+++ Week 1/moneyball/4.sql
@@ -1,4 +1,5 @@
 SELECT "players"."first_name", "players"."last_name", "salary", "player_id"
 FROM "salaries"
 JOIN "players" ON "salaries"."player_id" = "players"."id"
-ORDER BY "salary" ASC, "first_name" ASC, "last_name" ASC, "player_id" ASC;
+ORDER BY "salary" ASC, "first_name" ASC, "last_name" ASC, "player_id" ASC
+LIMIT 50;
