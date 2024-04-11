70483702d6130520787a52c931aa1ff44172d6fc
Kyle Español <kylekce@gmail.com>
2024-04-11 11:25:48 +0000

    Apr 11, 2024, 7:25 PM

--- Week 1/moneyball/12.sql
+++ Week 1/moneyball/12.sql
@@ -6,3 +6,12 @@ WHERE "performances"."year" = 2001 AND "H" > 0
 AND "performances"."year" = "salaries"."year"
 ORDER BY "dollars per hit" ASC, "first_name" ASC, "last_name" ASC
 LIMIT 10;
+
+SELECT "first_name", "last_name", "salary" / "RBI" as "dollars per RBI"
+FROM "players"
+JOIN "salaries" ON "players"."id" = "salaries"."player_id"
+JOIN "performances" ON "players"."id" = "performances"."player_id"
+WHERE "performances"."year" = 2001 AND "RBI" > 0
+AND "performances"."year" = "salaries"."year"
+ORDER BY "dollars per RBI" ASC, "first_name" ASC, "last_name" ASC
+LIMIT 10;
