337f107c8b1fe0cbe2d0b724dba67b44ff93af96
Kyle Español <kylekce@gmail.com>
2024-04-11 06:31:43 +0000

    Apr 11, 2024, 2:31 PM

--- Week 1/moneyball/10.sql
+++ Week 1/moneyball/10.sql
@@ -1,6 +1,7 @@
 SELECT "first_name", "last_name", "salaries"."salary", "salaries"."year", "performances"."HR"
 FROM "players"
 JOIN "salaries" ON "players"."id" = "salaries"."player_id"
-JOIN "performances" ON "players"."id" = "performances"."player_id"
-
+JOIN "performances" ON "salaries"."year" = "performances"."year"
+WHERE "players"."id" = 1315
+GROUP BY "salaries"."year"
 ORDER BY "players"."id" ASC, "salaries"."year" DESC, "performances"."HR" DESC, "salaries"."salary" DESC;
