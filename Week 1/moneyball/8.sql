7dc280dd53dc00809d81d309d57eea14e3208d85
Kyle Español <kylekce@gmail.com>
2024-04-11 06:03:00 +0000

    Apr 11, 2024, 2:03 PM

--- Week 1/moneyball/8.sql
+++ Week 1/moneyball/8.sql
@@ -1,3 +1,10 @@
 SELECT "salary"
 FROM "salaries"
-WHERE "year" = 2001 AND "player_id"
+WHERE "year" = 2001 AND "player_id" = (
+    SELECT "player_id"
+    FROM "performances"
+    WHERE "year" = 2001
+    GROUP BY "player_id"
+    ORDER BY SUM("HR") DESC
+    LIMIT 1
+);
