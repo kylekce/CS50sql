bacf551fe09ffb21ca8cf29991a5d6e5b49a8c61
Kyle Español <kylekce@gmail.com>
2024-04-13 07:52:39 +0000

    Apr 13, 2024, 3:52 PM

--- Week 2/donuts/schema.sql
+++ Week 2/donuts/schema.sql
@@ -1,3 +1,27 @@
 CREATE TABLE "ingredients" (
-    
+    "id",
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "per_unit" TEXT NOT NULL,
+    PRIMARY KEY ("id")
+)
+
+CREATE TABLE "donuts" (
+    "id",
+    "name" TEXT NOT NULL,
+    "gluten_free" INTEGER NOT NULL CHECK("gluten_free" IN (0, 1)),
+    "price" REAL NOT NULL,
+    "ingredients" INTEGER NOT NULL,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("ingredients") REFERENCES "ingredients" ("id")
+)
+
+CREATE TABLE "orders" (
+    "id",
+    "donut_id" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "total_price" REAL NOT NULL,
+    "order_date" TEXT NOT NULL,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("donut_id") REFERENCES "donuts" ("id")
 )
