566b8812a6b84443bd5015c9356f66c93d9dbcff
Kyle Español <kylekce@gmail.com>
2024-04-13 07:42:37 +0000

    Apr 13, 2024, 3:42 PM

--- Week 2/connect/schema.sql
+++ Week 2/connect/schema.sql
@@ -23,3 +23,21 @@ CREATE TABLE "companies" (
     PRIMARY KEY ("id")
 );
 
+CREATE TABLE "user_connections" (
+    "id",
+    "user_id" INTEGER NOT NULL,
+    "connection_id" INTEGER NOT NULL,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id"),
+    FOREIGN KEY ("connection_id") REFERENCES "users" ("id")
+);
+
+CREATE TABLE "school_connections" (
+    "id",
+    "user_id" INTEGER NOT NULL,
+    "school_id" INTEGER NOT NULL,
+    "graduation_year" TEXT NOT NULL,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id"),
+    FOREIGN KEY ("school_id") REFERENCES "schools" ("id")
+);
