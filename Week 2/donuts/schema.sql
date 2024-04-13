192c6efa123d2172399ba060492112954bad1fb5
Kyle Español <kylekce@gmail.com>
2024-04-13 08:08:55 +0000

    Apr 13, 2024, 4:08 PM

--- Week 2/donuts/schema.sql
+++ Week 2/donuts/schema.sql
@@ -4,15 +4,15 @@ CREATE TABLE "ingredients" (
     "price" REAL NOT NULL,
     "per_unit" TEXT NOT NULL,
     PRIMARY KEY ("id")
-)
+);
 
 CREATE TABLE "donuts" (
     "id",
     "name" TEXT NOT NULL,
     "gluten_free" INTEGER NOT NULL CHECK("gluten_free" IN (0, 1)),
     "price" REAL NOT NULL,
-    PRIMARY KEY ("id"),
-)
+    PRIMARY KEY ("id")
+);
 
 CREATE TABLE "donut_ingredients" (
     "id",
@@ -21,14 +21,14 @@ CREATE TABLE "donut_ingredients" (
     PRIMARY KEY ("id"),
     FOREIGN KEY ("donut_id") REFERENCES "donuts" ("id"),
     FOREIGN KEY ("ingredient_id") REFERENCES "ingredients" ("id")
-)
+);
 
 CREATE TABLE "customers" (
     "id",
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
     PRIMARY KEY ("id")
-)
+);
 
 CREATE TABLE "orders" (
     "id",
@@ -38,4 +38,4 @@ CREATE TABLE "orders" (
     PRIMARY KEY ("id"),
     FOREIGN KEY ("customer_id") REFERENCES "customers" ("id"),
     FOREIGN KEY ("donut_id") REFERENCES "donuts" ("id")
-)
+);
