f2e871b8aab63a5d4c21f4909f926fbb5cc4f55d
Kyle Español <kylekce@gmail.com>
2024-04-23 04:16:59 +0000

    Apr 23, 2024, 12:16 PM

--- Week 3/meteorites/import.sql
+++ Week 3/meteorites/import.sql
@@ -12,3 +12,19 @@
 -- );
 
 -- .import --csv --skip 1 meteorites.csv meteorites_temp
+
+CREATE TABLE "meteorites_temp" (
+    "id",
+    "name" TEXT, 
+    "nametype" TEXT,
+    "class" TEXT,
+    "mass" REAL,
+    "discovery" TEXT,
+    "year" INTEGER,
+    "lat" REAL,
+    "long" REAL,
+    PRIMARY KEY ("id")
+);
+
+INSERT INTO meteorites 
+SELECT * FROM meteorites_temp;
