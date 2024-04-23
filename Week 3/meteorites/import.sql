0273268c2fa72e971367eb4dbf8656e23af7b234
Kyle Español <kylekce@gmail.com>
2024-04-23 04:49:42 +0000

    Apr 23, 2024, 12:49 PM

--- Week 3/meteorites/import.sql
+++ Week 3/meteorites/import.sql
@@ -11,19 +11,6 @@ CREATE TABLE "meteorites_temp" (
     PRIMARY KEY ("id")
 );
 
-.import --csv --skip 1 meteorites.csv meteorites_temp
-
-UPDATE "meteorites_temp"
-SET "mass" = NULL, "year" = NULL, "lat" = NULL, "long" = NULL
-WHERE "mass" = 0 OR "year" = 0 OR "lat" = 0 OR "long" = 0;
-
-UPDATE "meteorites_temp"
-SET "mass" = ROUND("mass", 2), "lat" = ROUND("lat", 2), "long" = ROUND("long", 2)
-WHERE "mass" IS NOT NULL;
-
-DELETE FROM "meteorites_temp"
-WHERE "nametype" = 'Relict';
-
 CREATE TABLE "meteorites" (
     "id" INTEGER,
     "name" TEXT, 
@@ -37,9 +24,22 @@ CREATE TABLE "meteorites" (
     PRIMARY KEY ("id")
 );
 
+.import --csv --skip 1 meteorites.csv meteorites_temp
+
 INSERT INTO "meteorites" ("name", "nametype", "class", "mass", "discovery", "year", "lat", "long")
 SELECT "name", "nametype", "class", "mass", "discovery", "year", "lat", "long"
 FROM "meteorites_temp"
 ORDER BY "year" DESC, "name" ASC;
+
+UPDATE "meteorites"
+SET "mass" = NULL, "year" = NULL, "lat" = NULL, "long" = NULL
+WHERE "mass" = 0 OR "year" = 0 OR "lat" = 0 OR "long" = 0;
+
+UPDATE "meteorites"
+SET "mass" = ROUND("mass", 2), "lat" = ROUND("lat", 2), "long" = ROUND("long", 2)
+WHERE "mass" IS NOT NULL;
+
+DELETE FROM "meteorites"
+WHERE "nametype" = 'Relict';
 
 -- DROP TABLE "meteorites_temp";
