d6679260bd17957afec86bf81a2ca146d3233887
Kyle Español <kylekce@gmail.com>
2024-04-23 05:03:15 +0000

    Apr 23, 2024, 1:03 PM

--- Week 3/meteorites/import.sql
+++ Week 3/meteorites/import.sql
@@ -28,17 +28,17 @@ CREATE TABLE "meteorites" (
 DELETE FROM "meteorites_temp"
 WHERE "nametype" LIKE 'Relict';
 
+UPDATE "meteorites_temp"
+SET "mass" = ROUND("mass", 2), "lat" = ROUND("lat", 2), "long" = ROUND("long", 2)
+WHERE "mass" IS NOT NULL;
+
+UPDATE "meteorites_temp"
+SET "mass" = NULL, "year" = NULL, "lat" = NULL, "long" = NULL
+WHERE "mass" = 0 OR "year" = 0 OR "lat" = 0 OR "long" = 0;
+
 INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
 SELECT "name", "class", "mass", "discovery", "year", "lat", "long"
 FROM "meteorites_temp"
 ORDER BY "year" ASC, "name" ASC;
-
-UPDATE "meteorites"
-SET "mass" = ROUND("mass", 2), "lat" = ROUND("lat", 2), "long" = ROUND("long", 2)
-WHERE "mass" IS NOT NULL;
-
-UPDATE "meteorites"
-SET "mass" = NULL, "year" = NULL, "lat" = NULL, "long" = NULL
-WHERE "mass" = 0 OR "year" = 0 OR "lat" = 0 OR "long" = 0;
 
 DROP TABLE "meteorites_temp";
