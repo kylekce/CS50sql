b2da27b37b319021624a410d1867c0cafe6ff3a0
Kyle Español <kylekce@gmail.com>
2024-04-23 04:34:18 +0000

    Apr 23, 2024, 12:34 PM

--- Week 3/meteorites/import.sql
+++ Week 3/meteorites/import.sql
@@ -1,17 +1,32 @@
 CREATE TABLE "meteorites_temp" (
-    "name" TEXT, 
+    "name", 
     "id",
-    "nametype" TEXT,
-    "class" TEXT,
-    "mass" REAL,
-    "discovery" TEXT,
-    "year" INTEGER,
-    "lat" REAL,
-    "long" REAL,
+    "nametype",
+    "class",
+    "mass",
+    "discovery",
+    "year",
+    "lat",
+    "long",
     PRIMARY KEY ("id")
 );
 
 .import --csv --skip 1 meteorites.csv meteorites_temp
+
+UPDATE "meteorites_temp"
+SET "mass" = NULL, "year" = NULL, "lat" = NULL, "long" = NULL
+WHERE "mass" = '' OR "year" = '' OR "lat" = '' OR "long" = '';
+
+UPDATE "meteorites_temp"
+SET "mass" = ROUND("mass", 2), "lat" = ROUND("lat", 2), "long" = ROUND("long", 2)
+WHERE "mass" IS NOT NULL;
+
+DELETE FROM "meteorites_temp"
+WHERE "nametype" = 'Relict';
+
+
+
+
 
 CREATE TABLE "meteorites" (
     "id",
@@ -19,7 +34,7 @@ CREATE TABLE "meteorites" (
     "nametype" TEXT,
     "class" TEXT,
     "mass" REAL,
-    "discovery" TEXT,
+    "discovery" TEXT CHECK("discovery" IN ('Fell', 'Found'))
     "year" INTEGER,
     "lat" REAL,
     "long" REAL,
