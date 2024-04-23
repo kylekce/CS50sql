506baf352f1fd9d7991277fed2e1ce6d12d34557
Kyle Español <kylekce@gmail.com>
2024-04-23 03:39:37 +0000

    Apr 23, 2024, 11:39 AM

--- Week 3/dont-panic/hack.sql
+++ Week 3/dont-panic/hack.sql
@@ -1,9 +1,9 @@
 UPDATE "users" 
-SET "password" = "oops!" 
+SET "password" = "982c0381c279d139fd221fce974916e7" 
 WHERE "username" = "admin";
 
 DELETE FROM "logs"
-WHERE "new_password" = "oops!";
+WHERE "new_password" = "982c0381c279d139fd221fce974916e7";
 
 INSERT INTO "user_logs" ("type")
 VALUES ("admin", "e10adc3949ba59abbe56e057f20f883e");
