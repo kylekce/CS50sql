6cf6fa8f801a2b20c13a95d3630f9348ef7e6d3b
Kyle Español <kylekce@gmail.com>
2024-04-23 03:40:44 +0000

    Apr 23, 2024, 11:40 AM

--- Week 3/dont-panic/hack.sql
+++ Week 3/dont-panic/hack.sql
@@ -5,5 +5,5 @@ WHERE "username" = "admin";
 DELETE FROM "logs"
 WHERE "new_password" = "982c0381c279d139fd221fce974916e7";
 
-INSERT INTO "user_logs" ("type")
-VALUES ("admin", "e10adc3949ba59abbe56e057f20f883e");
+INSERT INTO "user_logs" ("username", "action", "timestamp")
+VALUES ('admin', 'new_password', CURRENT_TIMESTAMP);
