76e806f1a1973a43bd574a65be812bfb6ffabb66
Kyle Español <kylekce@gmail.com>
2024-05-09 10:23:27 +0000

    May 9, 2024, 6:23 PM

--- Week 4/census/total.sql
+++ Week 4/census/total.sql
@@ -1,3 +1,3 @@
 CREATE VIEW "total" AS
-SELECT SUM("families"), SUM("households"), SUM("population"), SUM("male"), SUM("female")
+SELECT SUM("families"), SUM("households") AS "households", SUM("population"), SUM("male"), SUM("female")
 FROM "census";
