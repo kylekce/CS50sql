0a20ea08de2b5b68b3c8df2d5e949b60875db2d4
Kyle Español <kylekce@gmail.com>
2024-05-09 10:12:20 +0000

    May 9, 2024, 6:12 PM

---
+++ Week 4/census/most_populated.sql
@@ -0,0 +1,5 @@
+CREATE VIEW "most_populated" AS
+SELECT "district", SUM("families"), SUM("households"), SUM("population"), SUM("male"), SUM("female")
+FROM "census"
+GROUP BY "district"
+ORDER BY SUM("population") DESC;
