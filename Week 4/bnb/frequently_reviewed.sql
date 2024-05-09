ba894a0ff25b3e9aecdaed3ec30dc6a3c181a7d0
Kyle Español <kylekce@gmail.com>
2024-05-09 11:25:32 +0000

    May 9, 2024, 7:25 PM

--- Week 4/bnb/frequently_reviewed.sql
+++ Week 4/bnb/frequently_reviewed.sql
@@ -1,7 +1,7 @@
 CREATE VIEW "frequently_reviewed" AS
-SELECT "listing_id", "property_type", "host_name", COUNT("*")
+SELECT "listing_id", "property_type", "host_name", COUNT("listing_id") AS "reviews"
 FROM "reviews"
 JOIN "listings" ON "reviews"."listing_id" = "listings"."id"
 GROUP BY "listing_id"
 ORDER BY "reviews" DESC, "property_type" ASC, "host_name" ASC
-LIMIT 10;
+LIMIT 100;
