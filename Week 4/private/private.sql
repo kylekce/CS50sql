514572dd8f649ab6193f6c20c319ef93dc86b4bf
Kyle Español <kylekce@gmail.com>
2024-05-09 10:58:39 +0000

    May 9, 2024, 6:58 PM

--- Week 4/private/private.sql
+++ Week 4/private/private.sql
@@ -3,81 +3,41 @@ SELECT "phrase"
 FROM "message";
 
 INSERT INTO "message" ("phrase")
-VALUES (
-    SELECT substr("sentence", 98, 4) 
-    FROM ( 
-        SELECT "sentence" 
-        FROM "sentences" 
-        WHERE "ID" = 14
-    )
-);
+SELECT substr("sentence", 98, 4) 
+FROM "sentences"
+WHERE "ID" = 14;
 
 INSERT INTO "message" ("phrase")
-VALUES (
-    SELECT substr("sentence", 3, 5) 
-    FROM ( 
-        SELECT "sentence" 
-        FROM "sentences" 
-        WHERE "ID" = 114
-    )
-);
+SELECT substr("sentence", 3, 5) 
+FROM "sentences"
+WHERE "ID" = 114;
 
 INSERT INTO "message" ("phrase")
-VALUES (
-    SELECT substr("sentence", 72, 9) 
-    FROM ( 
-        SELECT "sentence" 
-        FROM "sentences" 
-        WHERE "ID" = 618
-    )
-);
+SELECT substr("sentence", 72, 9) 
+FROM "sentences"
+WHERE "ID" = 618;
 
 INSERT INTO "message" ("phrase")
-VALUES (
-    SELECT substr("sentence", 7, 3) 
-    FROM ( 
-        SELECT "sentence" 
-        FROM "sentences" 
-        WHERE "ID" = 630
-    )
-);
+SELECT substr("sentence", 7, 3) 
+FROM "sentences"
+WHERE "ID" = 630;
 
 INSERT INTO "message" ("phrase")
-VALUES (
-    SELECT substr("sentence", 12, 5) 
-    FROM ( 
-        SELECT "sentence" 
-        FROM "sentences" 
-        WHERE "ID" = 932
-    )
-);
+SELECT substr("sentence", 12, 5) 
+FROM "sentences"
+WHERE "ID" = 932;
 
 INSERT INTO "message" ("phrase")
-VALUES (
-    SELECT substr("sentence", 50, 7) 
-    FROM ( 
-        SELECT "sentence" 
-        FROM "sentences" 
-        WHERE "ID" = 2230
-    )
-);
+SELECT substr("sentence", 50, 7) 
+FROM "sentences"
+WHERE "ID" = 2230;
 
 INSERT INTO "message" ("phrase")
-VALUES (
-    SELECT substr("sentence", 44, 10) 
-    FROM ( 
-        SELECT "sentence" 
-        FROM "sentences" 
-        WHERE "ID" = 2346
-    )
-);
+SELECT substr("sentence", 44, 10) 
+FROM "sentences"
+WHERE "ID" = 2346;
 
 INSERT INTO "message" ("phrase")
-VALUES (
-    SELECT substr("sentence", 14, 5) 
-    FROM ( 
-        SELECT "sentence" 
-        FROM "sentences" 
-        WHERE "ID" = 3041
-    )
-);
+SELECT substr("sentence", 14, 5) 
+FROM "sentences"
+WHERE "ID" = 3041;
