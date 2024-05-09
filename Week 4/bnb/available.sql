bef1b9881482742dd11cd59c9bd5a572acd874bf
Kyle Español <kylekce@gmail.com>
2024-05-09 11:19:45 +0000

    May 9, 2024, 7:19 PM

--- Week 4/bnb/available.sql
+++ Week 4/bnb/available.sql
@@ -1,4 +1,5 @@
 CREATE VIEW "available" AS
 SELECT "listing_id", "property_type", "host_name", "accommodates", "date"
-FROM "availabilities"
+FROM "availabilities" 
+JOIN "listings" ON "availabilities"."listing_id" = "listings"."id"
 WHERE "available" = 'TRUE';
--- Week 4/bnb/frequently_reviewed.sql
+++ Week 4/bnb/frequently_reviewed.sql
@@ -1,4 +1,6 @@
 CREATE VIEW "frequently_reviewed" AS
-SELECT "listing_id", "property_type", "host_name", "reviews"
+SELECT "listing_id", "property_type", "host_name", SUM("reviews")
 FROM "reviews"
+JOIN "listings" ON "reviews"."listing_id" = "listings"."id"
+GROUP BY "listing_id"
 ORDER BY "reviews" DESC, "property_type" ASC, "host_name" ASC;
