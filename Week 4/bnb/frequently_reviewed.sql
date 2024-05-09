cbe6ecda2a20abd4ba6781163b138223c29d5b71
Kyle Español <kylekce@gmail.com>
2024-05-09 11:21:02 +0000

    May 9, 2024, 7:21 PM

--- Week 4/bnb/frequently_reviewed.sql
+++ Week 4/bnb/frequently_reviewed.sql
@@ -3,4 +3,5 @@ SELECT "listing_id", "property_type", "host_name", SUM("reviews")
 FROM "reviews"
 JOIN "listings" ON "reviews"."listing_id" = "listings"."id"
 GROUP BY "listing_id"
-ORDER BY "reviews" DESC, "property_type" ASC, "host_name" ASC;
+ORDER BY "reviews" DESC, "property_type" ASC, "host_name" ASC
+LIMIT 10;
