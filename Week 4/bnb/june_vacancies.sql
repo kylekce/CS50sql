7e496ff7eef1926bed0714194863bca3975fecfe
Kyle Español <kylekce@gmail.com>
2024-05-09 11:27:15 +0000

    May 9, 2024, 7:27 PM

--- Week 4/bnb/june_vacancies.sql
+++ Week 4/bnb/june_vacancies.sql
@@ -2,5 +2,5 @@ CREATE VIEW "june_vacancies" AS
 SELECT "listing_id", "property_type", "host_name", COUNT("listing_id") AS "days vacant"
 FROM "availabilities"
 JOIN "listings" ON "availabilities"."listing_id" = "listings"."id"
-WHERE "date" LIKE '2023-06%'
+WHERE "date" LIKE '2023-06%' AND "available" = 'TRUE'
 GROUP BY "listing_id";
