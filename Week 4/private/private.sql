abe37883f8f5824de6eb8f207eea3d9291cdadd5
Kyle Español <kylekce@gmail.com>
2024-05-09 11:03:54 +0000

    May 9, 2024, 7:03 PM

--- Week 4/private/private.sql
+++ Week 4/private/private.sql
@@ -1,49 +1,47 @@
-CREATE TABLE "sentences" (
-    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "sentence" TEXT
-);`1    
+CREATE TABLE "sentences_temp" (
+    "phrase" TEXT
+);
 
-
-INSERT INTO "message" ("phrase")
+INSERT INTO "sentences_temp" ("phrase")
 SELECT substr("sentence", 98, 4) 
 FROM "sentences"
 WHERE "ID" = 14;
 
-INSERT INTO "message" ("phrase")
+INSERT INTO "sentences_temp" ("phrase")
 SELECT substr("sentence", 3, 5) 
 FROM "sentences"
 WHERE "ID" = 114;
 
-INSERT INTO "message" ("phrase")
+INSERT INTO "sentences_temp" ("phrase")
 SELECT substr("sentence", 72, 9) 
 FROM "sentences"
 WHERE "ID" = 618;
 
-INSERT INTO "message" ("phrase")
+INSERT INTO "sentences_temp" ("phrase")
 SELECT substr("sentence", 7, 3) 
 FROM "sentences"
 WHERE "ID" = 630;
 
-INSERT INTO "message" ("phrase")
+INSERT INTO "sentences_temp" ("phrase")
 SELECT substr("sentence", 12, 5) 
 FROM "sentences"
 WHERE "ID" = 932;
 
-INSERT INTO "message" ("phrase")
+INSERT INTO "sentences_temp" ("phrase")
 SELECT substr("sentence", 50, 7) 
 FROM "sentences"
 WHERE "ID" = 2230;
 
-INSERT INTO "message" ("phrase")
+INSERT INTO "sentences_temp" ("phrase")
 SELECT substr("sentence", 44, 10) 
 FROM "sentences"
 WHERE "ID" = 2346;
 
-INSERT INTO "message" ("phrase")
+INSERT INTO "sentences_temp" ("phrase")
 SELECT substr("sentence", 14, 5) 
 FROM "sentences"
 WHERE "ID" = 3041;
 
-CREATE TEMPORARY VIEW "message" AS
+CREATE VIEW "sentences_temp" AS
 SELECT "phrase" 
 FROM "sentences_temp";
