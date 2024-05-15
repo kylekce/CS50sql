d9a8bc00ab20f23e41740d81a944612f4093064e
Kyle Español <kylekce@gmail.com>
2024-05-15 02:52:40 +0000

    May 15, 2024, 10:52 AM

--- Week 5/snap/4.sql
+++ Week 5/snap/4.sql
@@ -1,5 +1,10 @@
 SELECT "username"
 FROM "users"
 JOIN "messages"
-ON "users"."id" == "messages"."from_user_id"
-WHERE "messages"."expires_timestamp" >= '2024-01-01'
+ON "users"."id" == "messages"."to_user_id"
+WHERE "to_user_id" == (
+    SELECT "to_user_id"
+    FROM "messages"
+    GROUP BY "from_user_id"
+    ORDER BY COUNT("from_user_id") DESC
+);
