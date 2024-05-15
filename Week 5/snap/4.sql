f6ab979c3cce46ad68b3a069176473bcfa0a70fc
Kyle Español <kylekce@gmail.com>
2024-05-15 03:00:05 +0000

    May 15, 2024, 11:00 AM

--- Week 5/snap/4.sql
+++ Week 5/snap/4.sql
@@ -3,4 +3,5 @@ FROM "users"
 JOIN "messages"
 ON "users"."id" == "messages"."to_user_id"
 GROUP BY "to_user_id"
-ORDER BY COUNT("from_user_id") DESC;
+ORDER BY COUNT("from_user_id") DESC, "username" ASC
+LIMIT 1;
--- Week 5/snap/5.sql
+++
@@ -1,5 +0,0 @@
-SELECT "to_user_id", COUNT("from_user_id") AS "count"
-FROM "messages"
-GROUP BY "to_user_id"
-ORDER BY "count" DESC
-LIMIT 10;
