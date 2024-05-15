f06d5349f80fdef81949d273cfecd37d81972826
Kyle Español <kylekce@gmail.com>
2024-05-15 03:09:54 +0000

    May 15, 2024, 11:09 AM

--- Week 5/snap/5.sql
+++ Week 5/snap/5.sql
@@ -1,12 +1,5 @@
-SELECT "friend_id"
-FROM "friends"
-WHERE "user_id" = (
-    SELECT "id"
-    FROM "users"
-    WHERE "username" = 'lovelytrust487'
-) AND "user_id" = (
-    SELECT "id"
-    FROM "users"
-    WHERE "username" = 'exceptionalinspiration482'
-)
-GROUP BY "user_id";
+SELECT f1.friend_id AS mutual_friend_id
+FROM friends f1
+JOIN friends f2 ON f1.friend_id = f2.friend_id
+WHERE f1.user_id = (SELECT user_id FROM users WHERE username = 'lovelytrust487')
+AND f2.user_id = (SELECT user_id FROM users WHERE username = 'exceptionalinspiration482');
