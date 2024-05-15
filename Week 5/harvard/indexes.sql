e36a6e4585d16a45922c68eb3dea292c3736d63a
Kyle Español <kylekce@gmail.com>
2024-05-15 03:26:31 +0000

    May 15, 2024, 11:26 AM

--- Week 5/harvard/indexes.sql
+++ Week 5/harvard/indexes.sql
@@ -1,1 +1,4 @@
-CREATE INDEX "semester_index" ON "courses" ("semester");
+CREATE INDEX "search_semester" ON "courses" ("semester");
+
+CREATE INDEX "search_department" ON "courses" ("department")
+WHERE "semester" = 'Fall 2023' OR "semester" = 'Spring 2024';
