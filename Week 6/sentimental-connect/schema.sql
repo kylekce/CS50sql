fb03e190bd0926408528909f8bbe2e0631653585
Kyle Español <kylekce@gmail.com>
2024-05-21 06:42:43 +0000

    May 21, 2024, 2:42 PM

--- Week 6/sentimental-connect/schema.sql
+++ Week 6/sentimental-connect/schema.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `users` (
     `id` INT AUTO_INCREMENT,
-    `first_name` TEXT NOT NULL,
-    `last_name` TEXT NOT NULL,
-    `password` TEXT NOT NULL,
+    `first_name` VARCHAR(32) NOT NULL,
+    `last_name` VARCHAR(32) NOT NULL,
+    `username` VARCHAR(32) NOT NULL,
+    `password` VARCHAR(128) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
