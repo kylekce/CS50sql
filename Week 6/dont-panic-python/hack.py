a199dcdbe6e507362a704174615377f9218414d5
Kyle Español <kylekce@gmail.com>
2024-05-21 15:44:32 +0800

    Update admin password in dont-panic-python hack.py script

--- Week 6/dont-panic-python/hack.py
+++ Week 6/dont-panic-python/hack.py
@@ -1,1 +1,19 @@
-print("Hacked!")
+from cs50 import SQL
+
+db = SQL("sqlite:///dont-panic.db")
+
+password = str(input("Enter the new admin password: "))
+
+if password:
+    db.execute(
+        """
+            UPDATE users
+            SET "password" = ?
+            WHERE username = 'admin'
+        """,
+        password,
+    )
+
+    print("Hacked!")
+else:
+    print("Something went wrong.")
